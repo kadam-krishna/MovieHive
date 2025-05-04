167bf6fad66ff28a42088e3b990e1e1afe7427fe
Kadam Krishna <krishnakadamwork@gmail.com>
2025-05-04 11:59:57 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
         throw new Error("Failed to fetch movies");
       }
       const data = await response.json();
-      console.log(data);
+      // console.log(data);
       if(data.Response === 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies');
         setMovieList([]);
